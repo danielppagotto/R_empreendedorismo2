ab5d2bd47e80e8d75a321a79f757d0b16d170736
danielppagotto <danielppagotto@gmail.com>
2021-07-01 14:26:35 -0300

    Update modulo2.R

--- scripts/modulo2.R
+++ scripts/modulo2.R
@@ -1,294 +1,144 @@
-
 # Curso de R 
-# Mod. 1 - Conceitos básicos 
+# Mod. 2 - R aplicado ao GEM 
 # Desenvolvido por Daniel do Prado Pagotto (LAPEI-UFG)
 
-# Os "#" representam um comentário. O R pula as linhas que tiverem esse #
+# Os "#" representam um comentario. O R pula as linhas que tiverem esse #
 # Para rodar cada linha, coloque o cursor sobre a linha do comando e 
-# aperte ctrl + Enter (ou cmd + Enter, para o usuários de Mac)
+# aperte ctrl + Enter (ou cmd + Enter, para o usuarios de Mac)
 
-# Os pacotes abaixo serão usados ao longo da nossa aula. Caso não tenha eles 
-# instalados no seu R, é necessário instalar usando os comandos abaixo: 
+# Os pacotes abaixo serao usados ao longo da nossa aula. Caso nao tenha eles 
+# instalados no seu R, e necessario instalar usando as instrucoes abaixo: 
 # 1) retire os # da frente de cada linha; 2) rode cada comando (ctrl + Enter)
 
-# install.packages("dplyr")
-# install.packages("ggplot2")
-# install.packages("skimr")
-# install.packages("forcats")
-# install.packages("gapminder")
-# install.packages("DT")
-# install.packages("plotly")
+#install.packages("ggrepel")
+#install.packages("directlabels")
+#install.packages("GGally")
+#install.packages("skimr")
+#install.packages("readr")
+#install.packages("dplyr")
+#install.packages("ggplot2")
 
-library(dplyr)
-library(ggplot2)
+library(readr) 
+library(dplyr) 
 library(skimr)
-library(forcats)
-library(gapminder)
+library(ggplot2) 
+library(ggrepel) 
+library(directlabels)
+library(GGally)
 
-# Parte 1 - Operacoes, atribuicoes e objetos ------------------------------
 
-# Operações básicas
 
-5 + 5
-10 - 6
-10*2
-5/2
-5**2
-sqrt(16)
-5*(50-45)
+#  Bases GEM-NES - National Expert Survey ---------------------------------
 
-#Atribuições
+#Vamos comecar lendo a base GEM a partir do meu repositorio do github:
 
-x <- 5 + 5
-y <- 10 - 16
-a <- 9
-soma <- a + x
-nome <- "daniel"
-certo <- TRUE
+gem_especialistas <- read_delim("https://raw.githubusercontent.com/danielppagotto/R_empreendedorismo2/main/arquivos%20de%20bases/gem_nes_historico.csv", ";", 
+                                escape_double = FALSE, trim_ws = TRUE)
+
+#Vamos usar o glimpse para conferir a base 
+glimpse(gem_especialistas)
 
-pesoDaniel <- 79
-alturaDaniel <- 1.78
-imcDaniel <- pesoDaniel/alturaDaniel**2
-
-
-# Trabalhando com vetores
-pesos <- c(65, 95, 75, 77, 80, 68)
-alturas <- c(1.60, 1.78, 1.80, 1.68, 1.72, 1.65)
-imc <- pesos/alturas**2
-imc
-
-help(round)
-round(imc, 2)
-
-imc <- round(imc, 2) #estou sobrescrevendo um vetor
-                     # arredondado sobre ele mesmo
-imc
-
-# Trabalhando com matrizes 
-Matriz<-cbind(pesos,alturas,imc)
-Matriz
-
-rownames(Matriz) <- c("Alice","Gilmar","Cecilia","Bianca","Valentina","Augusto")
-Matriz
-
-
-# Manipulação de dados  ---------------------------------------------------
-
-basePaises <- gapminder
-
-# inspecionando a estrutura da base
-str(basePaises)
-
-# inspecionando as 6 primeiras observações 
-head(basePaises)
-
-# inspecionando as 10 últimas observações
-tail(basePaises, n = 10)
-
-# estatísticas descritivas da base
-summary(basePaises)
-
-# Acessando uma variável da base
-basePaises$continent
-
-# Acessando elementos únicos
-unique(basePaises$year)
-
-# Média de um vetor
-mean(basePaises$lifeExp)
-
-
-# O pacote dplyr ----------------------------------------------------------
-
-glimpse(basePaises)
-
-# Filter 
-
-basePaises %>% 
-  filter(continent == "Asia")
-
-basePaises%>%
-  filter(continent == "Americas" & year>1990)
-
-# != diferente 
-basePaises%>%
-  filter(continent != "Oceania")
-
-# Você pode armazenar sua consulta em outro objeto
-baseAsia <- basePaises%>%
-  filter(continent == "Asia")
-
-# Select 
-basePaises%>%
-  select(year,country,gdpPercap)
-
-basePaises%>%
-  select(-lifeExp)
-
-basePaises %>%
-  filter(continent == "Americas" & year>1990) %>%
-  select(year,country,gdpPercap)
-
-# Mutate 
-
-basePaises <- basePaises %>%
-  mutate(GDP = gdpPercap * pop)
-
-basePorte <- basePaises %>%
-  filter(year == 1992) %>%
-  mutate(porte = if_else(pop>median(pop),"G","P"))
-
-head(basePorte)
-
-# Group_by + summarize 
-
-basePaises %>%
-  group_by(country) %>%
-  summarize(meanLE=mean(lifeExp),meanPop=mean(pop),
-            meanGpc=mean(gdpPercap))
-
-
-basePaises %>%
-  group_by(continent,year) %>%
-  summarize(meanLE=mean(lifeExp),meanPop=mean(pop),
-            meanGpc=mean(gdpPercap))
-
-# Top_n + arrange 
-
-basePaises %>% 
-  filter(year == 2007) %>% 
-  top_n(5,pop) %>% 
-  arrange(desc(pop))
-
-# Joins 
-
-# lendo a base munic a partir do meu repositorio do Github
-munic <- read.csv("https://raw.githubusercontent.com/danielppagotto/R_empreendedorismo1/main/arquivos%20de%20bases/politicas_empreendedorismo.csv",
-                  sep = ";", encoding = "UTF-8") %>% rename(cod_cidade = X.U.FEFF.CodMun)
-
-ice <- read.csv("https://raw.githubusercontent.com/danielppagotto/R_empreendedorismo1/main/arquivos%20de%20bases/base_ice.csv",
-                sep = ";", encoding = "UTF-8")
-
-inner <- munic %>% 
-  inner_join(ice, by = c("cod_cidade"="cod_ibge"))
-
-left <- munic %>% 
-  left_join(ice, by = c("cod_cidade"="cod_ibge"))
-
-# Visualização de dados ---------------------------------------------------
-
-# base usada
-base <- gapminder %>% 
-  filter(year == 2007) 
-
-glimpse(base)
-
-# Análise descritiva 
-skimr::skim(base)
-
-# Histograma - Rbase 
-hist(base$lifeExp)
-
-# Histograma - ggplot 
-ggplot(base, aes(x = lifeExp)) + geom_histogram()
-
-# Densidade - ggplot 
-base %>% 
-  ggplot(aes(x = lifeExp)) + geom_density()
-
-# Boxplot 
-base %>% 
-  ggplot(aes(y = lifeExp)) + geom_boxplot()
-
-base %>% 
-  ggplot(aes(x = continent, y = lifeExp)) + geom_boxplot()
-
-# Coluna 
-base %>% 
-  filter(continent == "Asia") %>% 
-  top_n(n = 10, wt = lifeExp) %>% 
-  ggplot(aes(x = country, y = lifeExp)) + geom_col()
-
-base %>% 
-  filter(continent == "Asia") %>% 
-  top_n(n = 10, wt = lifeExp) %>% 
-  ggplot(aes(x = country, y = lifeExp)) + geom_col() + coord_flip()
-
-base %>% 
-  filter(continent == "Asia") %>% 
-  top_n(n = 10, wt = lifeExp) %>% 
-  ggplot(aes(x = fct_reorder(country,lifeExp), y = lifeExp)) + geom_col() + coord_flip()
-
-base %>% 
-  filter(continent == "Asia") %>% 
-  top_n(n = 10, wt = lifeExp) %>% 
-  ggplot(aes(x = fct_reorder(country,lifeExp), y = lifeExp)) + geom_col() + 
-  geom_label(aes(label=round(lifeExp))) +  coord_flip()
-
-gapminder %>% 
-  filter(country == "Brazil") %>% 
-  ggplot(aes(x = year, y = lifeExp)) + geom_line()
-
-paises <- c("Brazil","Argentina")
-
-gapminder %>% 
-  filter(country %in% paises) %>% 
-  ggplot(aes(x = year, y = lifeExp, col = country)) + geom_line()
-
-base %>% 
-  top_n(10, gdpPercap) %>% 
-  ggplot(aes(x = fct_reorder(country,gdpPercap), 
-             y=gdpPercap, fill = continent)) + 
-  geom_col() + coord_flip()
-
-ggplot(base,aes(x=lifeExp)) + geom_density(fill="darkblue") +
-  labs(title = "Histograma da expectativa de vida", 
-       x = "Expectativa de vida", y = "Frequência") + theme_minimal()
-
-ggplot(base,aes(x=log(gdpPercap),y=lifeExp)) + geom_point() +
-  labs(x = "PIB per capita (log)", y = "Expectativa de vida") + theme_bw()
-
-ggplot(base,aes(y=lifeExp, x=log(gdpPercap))) + geom_point() +
-  geom_smooth(method = "lm", se=FALSE) + 
-  labs(x = "PIB per capita (log)",
-       y = "Expectativa de vida") + theme_bw()
-
-ggplot(base,aes(y=lifeExp, x=log(gdpPercap), col=continent)) + geom_point() +
-  geom_smooth(method = "lm", se=FALSE) + 
-  labs(x = "PIB per capita (log)",
-       y = "Expectativa de vida") + theme_bw()
-
-ggplot(base,aes(y=lifeExp, x=log(gdpPercap), col=continent,size = pop)) + geom_point() +
-  geom_smooth(method = "lm", se=FALSE) + 
-  labs(x = "PIB per capta (log)",
-       y = "Expectativa de vida") + theme_bw()
-
-ggplot(base,aes(x=log(gdpPercap), y=lifeExp, col=continent,
-                size = pop)) + geom_point() + geom_smooth(method = "lm", se=FALSE) + 
-  labs(x = "PIB per capita (log)", y = "Expectativa de vida") + theme_bw() + 
-  facet_grid(~continent)
-
-# Extra datatable 
-library(DT)
-
-DT::datatable(base, options = list(pageLength = 5), class = 'cell-border stripe')
-
-# Extra esquisse 
-library(esquisse)
-
-esquisser(viewer = "browser")
-
-# Extra plotly 
-
-library(plotly)
-
-ggplot(base,aes(y=lifeExp, x=log(gdpPercap), col=continent)) + geom_point() +
-  labs(x = "PIB per capita (log)",
-       y = "Expectativa de vida") + theme_bw()
-
-grafico <- ggplot(base,aes(y=lifeExp, x=log(gdpPercap), col=continent)) + geom_point() +
-  labs(x = "PIB per capita (log)",
-       y = "Expectativa de vida") + theme_bw()
-
-ggplotly(grafico)
+#Vamos baixar a base e comparar alguns paises
+paises <- c("Brazil","Chile","Colombia","Mexico")
+
+#vamos separar algumas variaveis de interesse
+gem_paises_selecionados <- gem_especialistas %>% 
+  filter(economy %in% paises) %>% 
+  select(economy, year, gov_support, taxes_bureaucracy, 
+         gov_programs, internal_market_dynamics)
+
+# Estatisticas descritivas
+gem_paises_selecionados %>%
+  select(-year) %>% 
+  group_by(economy) %>% 
+  skim()
+
+#Plotando variaveis
+gem_paises_selecionados %>% 
+  ggplot(aes(year,gov_support)) + geom_line()
+
+#Adicionando o parametro col
+gem_paises_selecionados %>% 
+  ggplot(aes(year,gov_support, col = economy)) + geom_line(size = 2) +
+  theme_minimal()
+
+#Adicionando titulos
+gem_paises_selecionados %>% 
+  ggplot(aes(year,gov_support, col = economy)) + geom_line(size = 2) +
+  theme_minimal() + xlab("Ano") + ylab("Suporte Governamental") +
+  ggtitle("Suporte Governamental", "Comparação entre Brasil, Chile, Colombia e México - Dados GEM")
+
+#Adicionando textos as linhas
+gem_paises_selecionados %>% 
+  ggplot(aes(year,gov_support, col = economy)) + geom_line(size = 2) +
+  geom_dl(aes(label = economy), method = list(dl.combine("first.points", "last.points"))) +
+  theme_minimal() + xlab("Ano") + ylab("Suporte Governamental") +
+  ggtitle("Suporte Governamental", "Comparação entre Brasil, Chile, Colombia e México - Dados GEM")
+
+#Aumentando fontes
+gem_paises_selecionados %>% 
+  ggplot(aes(year,gov_support, col = economy)) + geom_line(size = 2) +
+  geom_dl(aes(label = economy), method = list(dl.combine("first.points", "last.points"))) +
+  theme_minimal() + xlab("Ano") + ylab("Suporte Governamental") + theme(legend.position = "none") +
+  theme(plot.title = element_text(size=22)) + theme(axis.title.y = element_text(size = rel(1.2), angle = 90)) + theme(axis.title.x = element_text(size = rel(1.2), angle = 0)) + ggtitle("Suporte Governamental", "Comparação entre Brasil, Chile, Colombia e México - Dados GEM")
+
+#Mudando a escala
+gem_paises_selecionados %>% 
+  ggplot(aes(year,gov_support, col = economy)) + geom_line(size = 2) +
+  geom_dl(aes(label = economy), method = list(dl.combine("first.points", "last.points"))) +
+  theme_minimal() + xlab("Ano") + ylab("Suporte Governamental") + theme(legend.position = "none") +
+  theme(plot.title = element_text(size=22)) + theme(axis.title.y = element_text(size = rel(1.2), angle = 90)) + theme(axis.title.x = element_text(size = rel(1.2), angle = 0)) + ggtitle("Suporte Governamental", "Comparação entre Brasil, Chile, Colombia e México - Dados GEM") +
+  scale_x_continuous(breaks = seq(2010,2020,1))
+
+
+# Base APS - Adult Population Survey --------------------------------------
+
+# Juntando o GEM e a base WGIDATASET
+
+#Lendo as bases a partir do GitHub
+
+wgidataset <- read_csv("https://raw.githubusercontent.com/danielppagotto/R_empreendedorismo2/main/arquivos%20de%20bases/wgi.csv")
+
+gem_aps <- read_delim("https://raw.githubusercontent.com/danielppagotto/R_empreendedorismo2/main/arquivos%20de%20bases/gem_2019_aps.csv", ";", escape_double = FALSE, trim_ws = TRUE)
+
+#Juntando bases
+gem_wgid <- gem_aps %>% 
+  left_join(wgidataset, by = c("abrev" = "code")) %>% 
+  select(economy, continent, tea, perceived_opportunities,
+         fear_failure, established_ownership, 
+         entrepreneurship_as_good_carrer_choice,
+         corruption, rule_of_law, regulatory_quality, 
+         political_stability, voice_accountability)
+
+
+gem_wgid %>%
+  group_by(continent) %>% 
+  skim()
+
+#Estatistica descritiva - GGally
+gem_wgid %>% select(-economy,-continent) %>% 
+  ggpairs()
+
+#Explorando mais a fundo a variavel Entrepreneurship as good carrer choice
+gem_wgid %>% 
+  ggplot(aes(x = political_stability, y = entrepreneurship_as_good_carrer_choice)) +
+  geom_point() + theme_minimal() + geom_smooth(method = "lm", se = FALSE)
+
+gem_wgid %>% 
+  ggplot(aes(x = political_stability, y = entrepreneurship_as_good_carrer_choice)) +
+  geom_point(aes(col = continent, size = 1.5)) + 
+  theme_minimal() + geom_text_repel(aes(label = economy)) + 
+  theme(legend.position = "none") + geom_smooth(method = "lm", se = FALSE)
+
+#Aplicando facet_grid()
+gem_wgid %>% 
+  ggplot(aes(x = political_stability, y = entrepreneurship_as_good_carrer_choice)) +
+  geom_point(aes(col = continent, size = 1.5)) + facet_grid(~continent) +
+  theme_minimal() + theme(legend.position = "none") + geom_smooth(method = "lm", se = FALSE)
+
+#Filtrando de um continente apenas e verificando os resultados das variaveis
+gem_wgid %>% 
+  filter(continent == "Europa") %>% 
+  select(-economy,-continent) %>% 
+  ggpairs()
 
